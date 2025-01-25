2ec3ef88ab0952e155b483da098440f5d80edda5
eatplastic <117698474+eatpIastic@users.noreply.github.com>
2025-01-25 17:29:05 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 /// <reference types="../CTAutocomplete" />
 
+const MouseEvent = Java.type("net.minecraftforge.client.event.MouseEvent")
+
 const colorToCoords = {
   "Green": "49,44",
   "Red": "51,42",
@@ -11,23 +13,22 @@ const colorToCoords = {
 
 // start of this taken from bloom
 register(MouseEvent, (event) => {
-  const button = event.button
-  const state = event.buttonstate
+  const button = event.button;
+  const state = event.buttonstate;
 
   // Only activate on a right click key press
-  if (button !== 1 || !state) return
+  if (button !== 1 || !state) return;
 
-  let heldItemName = Player.getHeldItem()?.getName()?.split(" ");
+  let heldItemName = Player.getHeldItem()?.getName()?.removeFormatting()?.split(" ");
 
   if (heldItemName?.[0] !== "Corrupted" || heldItemName?.[2] !== "Relic") return;
 
   const block = Player.lookingAt();
-
-  if (block.getRegistryName() !== "minecraft:cauldron" && block.getRegistryName() !== "minecraft:anvil") return;
+  
+  if (block?.type?.getName() !== "Cauldron" && block?.type?.getName() !== "Anvil") return;
 
   const blockCoords = `${Math.trunc(block.getX())},${Math.trunc(block.getZ())}`;
-
-  if (blockCoords[heldItemName[1]] !== blockCoords) {
+  if (colorToCoords[heldItemName[1]] !== blockCoords && !Player.isSneaking()) {
     cancel(event);
   }
 });
